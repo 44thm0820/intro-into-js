c7ce77d93bc135b9c93a585c08fcb259a489e536
Rey <reymercado.usa@gmail.com>
2018-09-25 11:05:47 -0700

    Add files via upload

--- exercises/variables-data-types/src/app.js
+++ exercises/variables-data-types/src/app.js
@@ -2,18 +2,18 @@
 // Variables/Data Types
 function willReturnAString() {
     // Create a variable below called "theString" that contains any string:
-
+    let theString="this contains a string.";
     return theString;
 }
 
 function willReturnABoolean() {
     // Create a variable below called "theBoolean" that contains a boolean value:
-
+    let theBoolean=false;
     return theBoolean;
 }
 
 function willReturnANumber() {
     // Create a variable below called "theNumber" that contains a number value:
-
+    let theNumber=44;
     return theNumber;
 }
